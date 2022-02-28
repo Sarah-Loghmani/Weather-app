0d5c39610aabbdd3d5d694fc4c600e7267700846
sarah loghmani <sarah.loghmani80@gmail.com>
2022-02-28 23:49:26 +0330

    2

--- weather-app/scripts/app.js
+++ weather-app/scripts/app.js
@@ -1,1 +1,38 @@
 // All of our kind of DOM manipulations showing things on the page
+
+const form = document.querySelector('form');
+const card = document.querySelector('.card');
+const info = document.querySelect('.info');
+
+const updateCity = async (city) => {
+   
+    const cityData = await getCity(city)
+    const weather = await getWeather(cityData.Key);
+   
+
+
+    // return {
+    //     cityData: cityData,
+    //     weather: weather
+    // }
+
+    //object shorthand notation
+    return {cityData, weather}
+
+}
+
+form.addEventListener('submit', e =>{
+    //prevent default action
+    e.preventDefault();
+    console.dir(form);
+    // get city value
+    const city = form.city.value.trim();
+    // console.log(city);
+    form.reset();
+
+    updateCity(city)
+      .then(data => {console.log(data)})
+      .catch(err => {console.log(err)})
+
+}
+)
