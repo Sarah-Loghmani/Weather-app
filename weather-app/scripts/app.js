f674e54e2005e7b7764412a109347472b7bf665f
sarah loghmani <sarah.loghmani80@gmail.com>
2022-03-01 14:09:21 +0330

    1

--- weather-app/scripts/app.js
+++ weather-app/scripts/app.js
@@ -4,32 +4,41 @@ const form = document.querySelector("form");
 const card = document.querySelector(".card");
 const info = document.querySelector(".info");
 
-const updateUI = async(data)=>{
-    const cityData = data.cityData;
-    const weather = data.weather;
+const updateUI = (data) => {
+  //   const cityData = data.cityData;
+  //   const weather = data.weather;
+  //destructure properties
+  const { cityData, weather } = data;
+  //   console.log(cityData);
+  //   console.log(weather[0].Temperature.Metric.value);
 
-    info.innerHTML = `<h5>CITY NAME</h5>
-                <div>WEATHER CONDITIONS</div>
+  info.innerHTML = `<h5>${cityData.EnglishName}</h5>
+                <div>${weather[0].WeatherText}</div>
                 <div>
-                    <span>TEMP</span><span>&degC</span>
+                    <span>${weather[0].Temperature.Metric.Value}</span><span>&degC / </span>
+                    <span>${weather[0].Temperature.Imperial.Value}</span><span>&degF</span>
                 </div>`;
-}
-const updateCity = async (city) => {
+
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
+
+async function updateCity(city) {
   const cityData = await getCity(city);
   const weather = await getWeather(cityData.Key);
   // return {
   //     cityData: cityData,
   //     weather: weather
   // }
-
   //object shorthand notation
   return { cityData, weather };
-};
+}
 
 form.addEventListener("submit", (e) => {
   //prevent default action
   e.preventDefault();
-  console.dir(form);
+  //   console.dir(form);
   // get city value
   const city = form.city.value.trim();
   // console.log(city);
@@ -40,6 +49,6 @@ form.addEventListener("submit", (e) => {
       updateUI(data);
     })
     .catch((err) => {
-      console.log(err)
+      console.log(err);
     });
 });
