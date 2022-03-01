6de84a82d62332d964465cb9247395236f7525c1
sarah loghmani <sarah.loghmani80@gmail.com>
2022-03-01 14:13:16 +0330

    1

--- weather-app/scripts/app.js
+++ weather-app/scripts/app.js
@@ -3,14 +3,15 @@
 const form = document.querySelector("form");
 const card = document.querySelector(".card");
 const info = document.querySelector(".info");
+const timeImage = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 
 const updateUI = (data) => {
   //   const cityData = data.cityData;
   //   const weather = data.weather;
   //destructure properties
   const { cityData, weather } = data;
-  //   console.log(cityData);
-  //   console.log(weather[0].Temperature.Metric.value);
+  console.log(data);
 
   info.innerHTML = `<h5>${cityData.EnglishName}</h5>
                 <div>${weather[0].WeatherText}</div>
@@ -19,6 +20,21 @@ const updateUI = (data) => {
                     <span>${weather[0].Temperature.Imperial.Value}</span><span>&degF</span>
                 </div>`;
 
+  //update the day/night & icon images
+  const iconSrc = `icons/${weather[0].WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
+
+  let timeImageSrc = null;
+
+  if (weather[0].IsDayTime) {
+    timeImageSrc = "icons/day.svg";
+  } else {
+    timeImageSrc = "icons/night.svg";
+  }
+
+  timeImage.setAttribute("src", timeImageSrc);
+
+  //refreshing
   if (card.classList.contains("d-none")) {
     card.classList.remove("d-none");
   }
