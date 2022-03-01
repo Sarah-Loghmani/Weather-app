c7af1ea6978b6ebab6a14c44f52007162faa4ce3
sarah loghmani <sarah.loghmani80@gmail.com>
2022-03-01 11:20:02 +0330

    3

--- weather-app/scripts/app.js
+++ weather-app/scripts/app.js
@@ -1,38 +1,45 @@
 // All of our kind of DOM manipulations showing things on the page
 
-const form = document.querySelector('form');
-const card = document.querySelector('.card');
-const info = document.querySelect('.info');
+const form = document.querySelector("form");
+const card = document.querySelector(".card");
+const info = document.querySelector(".info");
 
+const updateUI = async(data)=>{
+    const cityData = data.cityData;
+    const weather = data.weather;
+
+    info.innerHTML = `<h5>CITY NAME</h5>
+                <div>WEATHER CONDITIONS</div>
+                <div>
+                    <span>TEMP</span><span>&degC</span>
+                </div>`;
+}
 const updateCity = async (city) => {
-   
-    const cityData = await getCity(city)
-    const weather = await getWeather(cityData.Key);
-   
+  const cityData = await getCity(city);
+  const weather = await getWeather(cityData.Key);
+  // return {
+  //     cityData: cityData,
+  //     weather: weather
+  // }
 
+  //object shorthand notation
+  return { cityData, weather };
+};
 
-    // return {
-    //     cityData: cityData,
-    //     weather: weather
-    // }
+form.addEventListener("submit", (e) => {
+  //prevent default action
+  e.preventDefault();
+  console.dir(form);
+  // get city value
+  const city = form.city.value.trim();
+  // console.log(city);
+  form.reset();
 
-    //object shorthand notation
-    return {cityData, weather}
-
-}
-
-form.addEventListener('submit', e =>{
-    //prevent default action
-    e.preventDefault();
-    console.dir(form);
-    // get city value
-    const city = form.city.value.trim();
-    // console.log(city);
-    form.reset();
-
-    updateCity(city)
-      .then(data => {console.log(data)})
-      .catch(err => {console.log(err)})
-
-}
-)
+  updateCity(city)
+    .then((data) => {
+      updateUI(data);
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+});
